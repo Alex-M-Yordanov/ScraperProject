d3bcf5fccc2573be010378c00a72e40541b1663c
Alex <59801171+Alex-M-Yordanov@users.noreply.github.com>
2024-11-01 12:51:01 +0200

    Update app.py

--- Python/app.py
+++ Python/app.py
@@ -1,11 +1,9 @@
 from flask import Flask, request, jsonify
-import subprocess
 from flask_cors import CORS
-import json
+from scraper import Scraper, parse_ardes, parse_emag, parse_technopolis
 
 app = Flask(__name__)
 CORS(app)
-
 
 @app.route('/search', methods=['POST'])
 def search():
@@ -17,59 +15,32 @@ def search():
         return jsonify({"error": "No keywords provided"}), 400
 
     # Run the scrapers with the keywords
-    scraper_results = run_scrapers(keywords)
+    scraper_results = run_scraper(keywords)
     
     return jsonify(scraper_results)
 
-# parsing stdout output and creating dictionaries
-def parse(output):
-     lines = output.splitlines()
-     products = []
-
-     for i in range(0, len(lines), 3):
-        if i + 2 < len(lines):
-            name = lines[i].strip()
-            price = lines[i + 1].strip()
-            link = lines[i + 2].strip()
-            product = {"name": name, "price": price, "link": link}
-            products.append(product)
-
-     return products
-
-
-
-def run_scrapers(keywords):
+def run_scraper(keywords):
         results={}
-    
-        # Running scraper scripts with the keywords as an argument
-        result_ardes = subprocess.run(['python', 'ardes_scr.py', keywords], capture_output=True, text=True,encoding='utf-8')
-        result_emag = subprocess.run(['python', 'emag_scr.py', keywords], capture_output=True, text=True,encoding='utf-8')
-        result_technopolis = subprocess.run(['python', 'technopolis_scr.py', keywords], capture_output=True, text=True,encoding='utf-8')
-        
-        try:
-            ardes_output = result_ardes.stdout
-            a_out=parse(ardes_output)
-        except json.JSONDecodeError as e:
-            print(f"Error decoding JSON from ardes_scr.py output: {e}")
-        
-        try:
-            emag_output = result_emag.stdout
-            e_out=parse(emag_output)
-        except json.JSONDecodeError as e:
-            print(f"Error decoding JSON from ardes_scr.py output: {e}")
-
-        try:
-            technopolis_output = result_technopolis.stdout
-            t_out=parse(technopolis_output)
-        except json.JSONDecodeError as e:
-            print(f"Error decoding JSON from ardes_scr.py output: {e}")
-        
-        # creating dictionary where each member is a list of dictionaries(products) 
-        results = {
-            'ardes.bg': a_out,
-            'emag.bg': e_out,
-            'technopolis.bg': t_out
+        sites = {
+        'ardes.bg': Scraper(base_url='https://ardes.bg/products?q=',
+                            format_symbol='%20',
+                            parse_function=parse_ardes),
+        'emag.bg': Scraper(base_url='https://www.emag.bg/search/',
+                           format_symbol='%20',
+                           parse_function=parse_emag),
+        'technopolis.bg': Scraper(base_url='https://www.technopolis.bg/bg/search/',
+                                  format_symbol='%20',
+                                  parse_function=parse_technopolis)
         }
+    
+        for site_name, scraper in sites.items():
+            soup = scraper.get_data(keywords)
+            if soup:
+                all_products = scraper.parse(soup)
+                filtered_products = [product for product in all_products if "Смартфон" in product.get("title", "")]
+                results[site_name] = filtered_products
+            else:
+                results[site_name] = []
 
         return results
 
